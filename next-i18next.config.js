c4b9b7f666cad45620e417f03b1eeab6ea7b66b0
Esherloon <jinoo2005609@gmail.com>
2022-02-11 12:19:57 +0900

    Add ko-KR to next-i18next.config.js

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -2,8 +2,8 @@ const path = require('path')
 
 module.exports = {
   i18n: {
-    defaultLocale: 'en',
-    locales: ['en', 'zh-CN']
+    defaultLocale: 'ko-KR',
+    locales: ['en', 'ko-KR']
   },
   localePath: path.resolve('public/locales'),
   reloadOnPrerender: process.env.NODE_ENV === 'development',
