76fbd9f9e000845af14e64646c9a2df9145476cc
jinoo2005609 <69671333+jinoo2005609@users.noreply.github.com>
2022-04-25 15:40:49 +0900

    Block robots index

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,7 +6,8 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
-          <meta name="description" content="Esherloon's Drive (에셜룬의 드라이브)" />
+          <meta name="description" content="Esherloon's Other Drive (에셜룬의 다른 드라이브)" />
+          <meta name="robots" content="none" />
           <link rel="icon" href="/favicon.ico" />
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
